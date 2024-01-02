2f159281ae0b2df8625f10273d7ce29410a7cc78
mostafa <mostafa51mokyar@gmail.com>
2024-01-02 01:12:24 -0800

    5-print_comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
-# !/usr/bin/python3
-for i in range(0, 99):
+#!/usr/bin/python3
+for i in range(100):
     print('0{ii}'.format(ii=i) if i < 10 else f'{i}', end='')
     print(', ' if i != 99 else '\n', end='')
