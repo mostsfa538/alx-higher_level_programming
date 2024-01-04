2a62bc7199f04f2de8d1d2ce6f69dfd85d364ab0
mostafa <mostafa51mokyar@gmail.com>
2024-01-04 06:55:44 -0800

    100-my_calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    a = int(sys.argv[0])
-    sign = sys.argv[1]
-    b = int(sys.argv[2])
+    a = int(sys.argv[1])
+    sign = sys.argv[2]
+    b = int(sys.argv[3])
 
     if sign == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
