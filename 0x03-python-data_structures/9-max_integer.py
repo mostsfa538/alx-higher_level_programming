e4114415bb68b876e051f178d8b77036810e73d2
mostafa <mostafa51mokyar@gmail.com>
2024-01-05 00:45:16 -0800

    9-max_integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    ma = my_list[0]
+    if len(my_list):
+        return None
+    for i in my_list:
+        if i > ma:
+            ma = i
+    return ma
