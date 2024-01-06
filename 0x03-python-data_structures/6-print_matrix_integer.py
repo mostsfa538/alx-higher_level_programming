97a88480f077e3cb7075a66a712030e71b9c624f
mostafa <mostafa51mokyar@gmail.com>
2024-01-06 01:58:10 -0800

    6-print_matrix_integer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i in row:
-            print(i, end='')
-        print()
+    for i in matrix:
+        for idx, j in enumerate(i):
+            print("{:d}".format(j), end=' ' if idx != len(i) - 1 else '')
+        print("")
