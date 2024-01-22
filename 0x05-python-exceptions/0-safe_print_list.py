a7823bdbd22cde7d504299787a8d9fd2929bf20a
mostsfa538 <mostafa51moktar@gmail.com>
2024-01-22 11:48:36 +0200

    0-safe_print_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         while i < x:
             print(my_list[i], end='')
             i += 1
-        print(i)
+        print('')
         return i
     except IndexError:
         print('')
