fefc01108bbabc450893c722599918a498b89ffd
mostsfa538 <mostafa51moktar@gmail.com>
2024-01-29 18:11:19 +0200

    8-rectangle

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -99,14 +99,12 @@ class Rectangle:
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """who is bigger"""
-        if type(rect_1) is not Rectangle:
+        if not isinstance(rect_1, Rectangle):
             raise TabError('rect_1 must be an instance of Rectangle')
-        if type(rect_2) is not Rectangle:
+        if not isinstance(rect_2, Rectangle):
             raise TabError('rect_2 must be an instance of Rectangle')
 
-        a1 = rect_1.area()
-        a2 = rect_2.area()
-        if a1 >= a2:
+        if rect_1.area() >= rect_1.area():
             return rect_1
         else:
             return rect_2
