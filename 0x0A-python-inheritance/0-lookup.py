1044eb4a5bcf873edcbbc373caa5bcff0099dce7
mostsfa538 <mostafa51moktar@gmail.com>
2024-02-05 12:15:10 +0200

    Revert "0-lookup"
    This reverts commit e218082dc66d131172da574e50edb429e61d547e.

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""a function """
+"""lookup"""
 
 
 def lookup(obj):
-    """
-    Returns a list of all available attributes and methods of an object
-    """
-
+    """returns the list of available attributes and methods of an object"""
     return dir(obj)
