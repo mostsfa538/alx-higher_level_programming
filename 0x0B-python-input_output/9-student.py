b0022e7af02720fce5a19ea9b9cdf4acf6792765
mostsfa538 <mostafa51moktar@gmail.com>
2024-02-07 12:08:39 +0200

    9-student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a class"""
+
+
+class Student:
+    """a class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation"""
+        return (self.__dir__)
