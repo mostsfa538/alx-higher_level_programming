35aa7295bbb7bc64a4a4b87dd2304da67824b8cb
mostsfa538 <mostafa51moktar@gmail.com>
2024-02-07 11:51:43 +0200

    6-load_from_json_file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""import json"""
+import json
+
+
+def load_from_json_file(filename):
+    """creates an Object from a “JSON file"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.loads(f.read())
