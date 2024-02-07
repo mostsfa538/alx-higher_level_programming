4c13bfd8fe1bd061b4c8bc5a1e13a56a2010c5ea
mostsfa538 <mostafa51moktar@gmail.com>
2024-02-07 12:15:36 +0200

    11-student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""a class"""
+
+
+class Student:
+    """a class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Constructor"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation"""
+        if attrs is None:
+            return (self.__dict__)
+        else:
+            dict = {}
+            for i in attrs:
+                if hasattr(self, i):
+                    dict[i] = getattr(self, i)
+            return dict
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+
+        new = vars(self)
+        for key, value in json.items():
+            new[key] = value
