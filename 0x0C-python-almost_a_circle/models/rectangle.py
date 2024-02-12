14577a7cac9a273f74b25d6e97062f7d79060882
mostsfa538 <mostafa51moktar@gmail.com>
2024-02-12 19:16:00 +0200

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,7 +77,7 @@ class Rectangle(Base):
         for _ in range(self.__y):
             print()
             for _ in range(self.__height):
-                print(' ' + self.__x + '#' + self.__width)
+                print(' ' * self.__x + '#' * self.__width)
 
     def __str__(self):
         """overriding the __str__ method"""
