f3a1ca8b389c85f5a18994db335234c277c47d8d
mostsfa538 <mostafa51moktar@gmail.com>
2024-03-12 11:32:53 +0200

    best sscore

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- display both the score and the name with a score >= 10
-SELECT score, name FROM second_table WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
