7fce2b39a87dd690581fc900985f596fbb09dc63
mostsfa538 <mostafa51moktar@gmail.com>
2024-03-12 13:10:22 +0200

    top city

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- displays the average temperature (Fahrenheit) by city ordered by temperature (descending) TOP 3
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
-WHERE MONTH(date) IN (7, 8)
+WHERE MONTH(month) IN (7, 8)
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
