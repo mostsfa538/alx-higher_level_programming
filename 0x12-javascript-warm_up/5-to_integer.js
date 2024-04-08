1ebcf457d3f7804a370cb0cc1dcfc7f42d8b564a
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-08 18:16:49 +0200

    5-to_-integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+const number = parseInt(argv[2]);
+
+if (Number.isInteger(number)) {
+    console.log('My number: ' + number);
+} else {
+    console.log('Not a number');
+}
