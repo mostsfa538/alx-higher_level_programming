1f7c6cddf78074073d5f29074f20b4c100714444
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-08 21:18:30 +0200

    10-factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const factorial = (number) => {
+  if (number === 1) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
+};
+
+const { argv } = require('process');
+
+if (argv.length < 3) {
+  console.log(1);
+} else {
+  console.log(factorial(argv[2]));
+}
