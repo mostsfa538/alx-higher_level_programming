f5b5740e96b43cb9c1677a9b643a545bc7287e11
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-08 17:39:18 +0200

    1-new line

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+const c = 'C is fun';
+const python = 'Python is cool';
+const javaScript = 'JavaScript is amazing';
+console.log(c);
+console.log(python);
+console.log(javaScript);
