5b8967f2901f34c07f90168cdf985d46266e7936
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-08 21:27:13 +0200

    10-factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
-const factorial = (number) => {
+function factorial (number) {
   if (isNaN(number) || number === 0) {
     return 1;
   } else {
     return number * factorial(number - 1);
   }
-};
-
+}
 const { argv } = require('process');
 
 const n = parseInt(argv[2]);
