b9b51a4f8ac4ac25030d2b736f73d8a689418c3c
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-09 16:34:11 +0200

    more rectangle

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -20,23 +20,12 @@ class Rectangle {
   }
 
   rotate () {
-    for (let i = 0; i < this.width + this.width; i++) {
-      let rectRotate = '';
-      for (let i = 0; i < this.height + this.height; i++) {
-        rectRotate += 'X';
-      }
-      console.log(rectRotate);
-    }
+    [this.width, this.height] = [this.height, this.width];
   }
 
   double () {
-    for (let i = 0; i < this.height * 2; i++) {
-      let rectDouble = '';
-      for (let i = 0; i < this.width * 2; i++) {
-        rectDouble += 'X';
-      }
-      console.log(rectDouble);
-    }
+    this.height *= 2;
+    this.width *= 2;
   }
 }
 
