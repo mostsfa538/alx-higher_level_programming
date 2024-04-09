29161cb27dc371f0bfe002411a8267b534003e48
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-09 16:18:26 +0200

    more rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,43 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if ((isNaN(w) || isNaN(h)) || w <= 0 || h <= 0) {
+      return 'Rectangle {}';
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rect = '';
+      for (let i = 0; i < this.width; i++) {
+        rect += 'X';
+      }
+      console.log(rect);
+    }
+  }
+
+  rotate () {
+    for (let i = 0; i < this.width * 2; i++) {
+      let rectRotate = '';
+      for (let i = 0; i < this.height * 2; i++) {
+        rectRotate += 'X';
+      }
+      console.log(rectRotate);
+    }
+  }
+
+  double () {
+    for (let i = 0; i < this.height * 2; i++) {
+      let rectDouble = '';
+      for (let i = 0; i < this.width * 2; i++) {
+        rectDouble += 'X';
+      }
+      console.log(rectDouble);
+    }
+  }
+}
+
+module.exports = Rectangle;
