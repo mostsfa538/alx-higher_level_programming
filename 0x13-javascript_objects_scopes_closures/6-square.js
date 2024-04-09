d09bf27c58cbee05b2b33d72481d17dbccd5a0ab
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-09 16:58:06 +0200

    more Square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,35 +1,8 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if ((isNaN(w) || isNaN(h)) || w <= 0 || h <= 0) {
-      return 'Rectangle {}';
-    }
-    this.width = w;
-    this.height = h;
-  }
+const oldSquare = require('./5-square');
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let rect = '';
-      for (let i = 0; i < this.width; i++) {
-        rect += 'X';
-      }
-      console.log(rect);
-    }
-  }
-
-  rotate () {
-    [this.width, this.height] = [this.height, this.width];
-  }
-
-  double () {
-    this.height *= 2;
-    this.width *= 2;
-  }
-}
-
-class Square extends Rectangle {
+class Square extends oldSquare {
   constructor (size) {
     super(size, size);
   }
@@ -39,14 +12,13 @@ class Square extends Rectangle {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-      let rec = '';
+      let rect = '';
       for (let j = 0; j < this.width; j++) {
-        rec += c;
+        rect += c;
       }
-      console.log(rec);
+      console.log(rect);
     }
   }
 }
 
-module.exports = Rectangle;
 module.exports = Square;
