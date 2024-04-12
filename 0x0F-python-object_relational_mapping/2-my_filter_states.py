fbc7cb2523a1c10d6cf5f9638febf41a079ad6b8
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-12 20:05:30 +0200

    displays all values where name matches the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Here goes every thing"""
+import MySQLdb
+import sys
+
+
+def filter_states(username, password, database, name):
+    """
+        takes in an argument and displays all values in the states table of
+        hbtn_0e_0_usa where name matches the argument
+    """
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC", (name,))
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    name = sys.argv[4]
+    filter_states(username, password, database, name)
