94d3d0650bf0e960f4f48de0bbb8f49dc2e15aa3
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-13 13:08:08 +0200

    model state update id 2

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Here goes everything"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    update_state = session.query(State).filter(State.id == 2).first()
+
+    if update_state is not None:
+        update_state.name = 'New Mexico'
+
+    session.close()
