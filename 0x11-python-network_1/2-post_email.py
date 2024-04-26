693bd16ec2d557f8443fd942e25d1b08cfef43a0
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-26 16:17:09 +0300

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,7 +3,8 @@
    URL with the email as a parameter,
    and displays the body of the response (decoded in utf-8)"""
 import sys
-import urllib
+import urllib.parse
+import urllib.request
 
 
 if __name__ == "__main__":
