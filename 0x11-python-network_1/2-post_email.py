00f649dd4ecc765a040415560d07edcd4137c5ee
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-26 16:13:32 +0300

    post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email, sends a POST request to the passed
+   URL with the email as a parameter,
+   and displays the body of the response (decoded in utf-8)"""
+import sys
+import urllib
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'enail': email})
+    data = data.encode('utf-8')
+
+    with urllib.request.urlopen(url, data=data) as response:
+        print(response.read().decoded('utf-8'))
