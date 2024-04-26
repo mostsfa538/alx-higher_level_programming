5cfd9341bcf3733d511f5c1fdaac1feabf61bc11
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-26 16:15:01 +0300

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    data = urllib.parse.urlencode({'enail': email})
+    data = urllib.parse.urlencode({'email': email})
     data = data.encode('utf-8')
 
     with urllib.request.urlopen(url, data=data) as response:
