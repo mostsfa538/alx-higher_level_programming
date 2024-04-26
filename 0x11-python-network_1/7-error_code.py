8e03465bc4dd133d545152466e0db5bbab7bad00
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-26 17:14:26 +0300

    error code using requests package

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     r = requests.get(url)
-    if r.status_code == 2:
-        print(r.text)
-    else:
+    if r.status_code >= 400:
         print(f"Error code: {r.status_code}")
+    else:
+        print(r.text)
