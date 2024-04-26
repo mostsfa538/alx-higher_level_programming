167e75bb98b80b6317fe0aa6093e899c866f652d
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-26 16:52:39 +0300

    hbtn_header using requets package

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" script that takes in a URL, sends a request to the URL and displays
+    the value of the X-Request-Id variable found in the header of the response
+    using requests package
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    print(r.headers['X-Request-Id'])
