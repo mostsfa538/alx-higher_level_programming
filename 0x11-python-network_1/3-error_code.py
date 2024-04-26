39a098080768627d42ad9d0a231ffa0795e80928
mostsfa538 <mostafa51moktar@gmail.com>
2024-04-26 16:27:53 +0300

    error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,7 +2,8 @@
 """script that takes in a URL, sends a request to the URL and displays
    the body of the response (decoded in utf-8)"""
 import sys
-import urllib
+import urllib.request
+import urllib.error
 
 
 if __name__ == "__main__":
@@ -13,4 +14,4 @@ if __name__ == "__main__":
             body = response.read().decode('utf-8')
             print(body)
     except urllib.error.HTTPError as e:
-        print("Error code:", e.code)
+        print("Error code: ", e.code)
