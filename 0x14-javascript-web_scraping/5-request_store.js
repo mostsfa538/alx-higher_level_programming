835e4deb5d070a8725c65450dbc6829a8370fc99
mostsfa538 <mostafa51moktar@gmail.com>
2024-05-23 13:26:06 +0300

    request_store

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const path = process.argv[3];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  fs.writeFile(path, body, 'utf-8' ,(err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
