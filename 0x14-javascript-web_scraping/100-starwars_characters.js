0e129c1b871253401204e1a3d9e0abda1372cae5
mostsfa538 <mostafa51moktar@gmail.com>
2024-05-23 15:12:03 +0300

    100-starwars_characters

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
+
+request(url, (err, reponse, body) => {
+  if (err) {
+    console.log(err);
+  }
+
+  const movData = JSON.parse(body);
+  const chars = movData.characters;
+
+  for (const charLink of chars) {
+    request(charLink, (err, response, body) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      const charName = JSON.parse(body).name;
+
+      console.log(charName);
+    });
+  }
+});
